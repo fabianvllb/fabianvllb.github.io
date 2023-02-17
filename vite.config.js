7b9ac60c816c5b3e16ed366ae250ae6265f7fa3e
Fabian Villalobos <fabian.villalobos@alumnos.upm.es>
2023-02-17 16:09:45 +0100

    changing base url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/personal-page/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
