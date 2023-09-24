2fc0bfe0435e8f048324bc811642838275af92ee
Fabian Villalobos <fabian.villalobos@alumnos.upm.es>
2023-09-24 21:10:10 +0200

    adding base url to vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/fabianvllb.github.io/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
